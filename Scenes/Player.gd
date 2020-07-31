84c140f4cd6df63d3a83e5c929701242e4bf86e6
Alex Kharouk <akharoukpp@gmail.com>
2020-07-31 13:15:58 +0200

    Get basic moving down

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -6,8 +6,10 @@ onready var animationPlayer = $Mac/AnimationPlayer
 var velocity = Vector2()
 
 func _physics_process(delta):
-	animationPlayer.play("Idle")
 	get_input()
+	var animation = "Walk" + direction()
+	animationPlayer.play(animation)
+	
 	velocity = move_and_slide(velocity)
 
 func get_input():
@@ -18,7 +20,18 @@ func get_input():
 		velocity.x -= 1
 	if Input.is_action_pressed('ui_down'):
 		velocity.y += 1
-		animationPlayer.play("WalkDown")
 	if Input.is_action_pressed('ui_up'):
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
+
+func direction():
+	if Input.is_action_pressed("ui_right"):
+		return "Right"
+	elif Input.is_action_pressed('ui_left'):
+		return "Left"
+	elif Input.is_action_pressed('ui_down'):
+		return "Down"
+	elif Input.is_action_pressed('ui_up'):
+		return "Up"
+	else:
+		return "Idle"
