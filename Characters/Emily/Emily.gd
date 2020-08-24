ef63a0552fe027c7f29863542c7e39f43fd5204e
Alex Kharouk <akharoukpp@gmail.com>
2020-08-24 12:28:37 +0200

    Set up the basic conversation

--- Characters/Emily/Emily.gd
+++ Characters/Emily/Emily.gd
@@ -1,14 +1,19 @@
 extends StaticBody2D
 
 export (String, FILE, "*.json") var dialogue_file : String
-var finished := false
+var started_dialogue := false
 var player
 
 func _ready():
 	player = get_tree().root.get_node("World/Player")
 	print(player)
+	
 	Dialogue.connect("dialogue_ended", self, "_on_dialogue_end")
 
 func talk():
-	Dialogue.start_dialogue(dialogue_file)
-	print("Talking!")
+	if started_dialogue == false:
+		Dialogue.start_dialogue(dialogue_file)
+		started_dialogue = true
+
+func _on_dialogue_end():
+	started_dialogue = false
